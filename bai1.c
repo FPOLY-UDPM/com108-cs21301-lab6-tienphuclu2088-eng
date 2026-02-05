a853e1f53e1d59f729e1ea51b5c85a56e0560e13
tienphuclu2088-eng <tienphuclu2088@gmail.com>
2026-02-05 08:48:04 +0700

    nop bai

--- bai1.c
+++ bai1.c
@@ -1,13 +1,48 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Lưu Phúc Tiến]
+ * MSSV:      [PS48200]
+ * Lớp:       [COM108-CS21301]
  *****************************************************************************/
 
 //  BÀI 1: TÍNH TRUNG BÌNH TỔNG CÁC SỐ CHIA HẾT CHO 3 TRONG MẢNG 
 //  Input: Nhập vào từ bàn phím 1 mảng các số nguyên. Mảng gồm n phần tử 
 //  Output: Xuất ra màn hình kết quả của trung bình tổng các số chia hết cho 3 trong mảng 
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Nhap so phan tu cua mang: ");
+    scanf("%d", &n);
+    
+    int mang[n];
+    int i;
+    
+
+    for(i = 0; i < n; i++) {
+        printf("Nhap phan tu thu %d: ", i + 1);
+        scanf("%d", &mang[i]);
+    }
+    
+    float tong = 0;
+    float tb;
+    int count = 0;
+    
+    for(i = 0; i < n; i++) {
+        if(mang[i] % 3 == 0) {
+            tong += mang[i];   
+            count++;          
+        }
+    }
+    
+  
+    if(count > 0) {
+        tb = tong / count;
+        printf("Trung binh tong cac so chia het cho 3 trong mang la: %.2f\n", tb);
+    } else {
+        printf("Khong co so nao chia het cho 3 trong mang!\n");
+    }
+    
+    return 0;
+}
 
 
-// VIẾT CODE Ở ĐÂY
 
